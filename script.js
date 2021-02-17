72080866898a5da059563d119e1ac2555efc3835
Samantha Scibetta <53345649+scibettas1@users.noreply.github.com>
2021-02-17 17:13:01 -0500

    now recognizes if the boxes are checked, but still need to set up the search filter

--- script.js
+++ script.js
@@ -11,8 +11,8 @@ $(document).ready(() => {
         const result = JSON.parse(res);
         console.log(result)
 
-        const aptArray = result.map((apt) => {
-            
+        var aptArray = result.map((apt) => {
+
             if (apt.Beds == 0) {
                 var beds = "Studio";
             } else {
@@ -25,18 +25,31 @@ $(document).ready(() => {
             const rent = "$" + apt.MinimumRent + "-$" + apt.MaximumRent;
             const plan = "<img src=https://cdn.rentcafe.com/dmslivecafe/2/70799/" + apt.FloorplanImageName + " class='img-fluid'>";
 
-            const aptLi = "<div class='card shadow p-3 mb-5 bg-white rounded'><div class='row border-bottom'><h1 class='text-center'>" + 
-            name + "</h1></div><div class='row'><div class='col-md-8'>" + plan + "</div><div class='col-md-4 details'><div class='row'>Beds: " + 
-            beds + "</div><div class='row'>Baths: " + baths + "</div><div class='row'>SqFt: " + 
-            sqft + "</div><div class='row'>Rent: " + rent + "</div></div></div></div>";
-            
-const availability = JSON.parse(apt.AvailableUnitsCount)
-console.log(availability)
+            const aptLi = "<div class='card shadow p-3 mb-5 bg-white rounded'><div class='row border-bottom'><h1 class='text-center'>" +
+                name + "</h1></div><div class='row'><div class='col-md-8'>" + plan + "</div><div class='col-md-4 details'><div class='row'>Beds: " +
+                beds + "</div><div class='row'>Baths: " + baths + "</div><div class='row'>SqFt: " +
+                sqft + "</div><div class='row'>Rent: " + rent + "</div></div></div></div>";
+
+
+            //This only displays units if they are available
+            const availability = JSON.parse(apt.AvailableUnitsCount)
+            //console.log(availability)
 
             if (availability > 0) {
                 aptEl.append(aptLi);
-              }
-
+            }
+            // if studio box is checked
+            // if () {
+            //     aptEl.append(aptLi);
+            // }
+            // //if 1 bedroom box is checked
+            // if () {
+            //     aptEl.append(aptLi);
+            // }
+            // //if 2 bedroom box is checked
+            // if () {
+            //     aptEl.append(aptLi);
+            // }
 
             return {
                 name: apt.FloorplanName,
@@ -47,8 +60,22 @@ console.log(availability)
                 plan: "<img src=https://cdn.rentcafe.com/dmslivecafe/2/70799/" + apt.FloorplanImageName + ">",
                 availability: apt.AvailableUnitsCount
             }
+
         })
-        console.log(aptArray)
+        function getSelectedCheckboxValues(name) {
+            const checkboxes = document.querySelectorAll(`input[name="${name}"]:checked`);
+            let values = [];
+            checkboxes.forEach((checkbox) => {
+                values.push(checkbox.value);
+            });
+            return values;
+        }
+        
+        const btn = document.querySelector('#btn');
+        btn.addEventListener('click', (event) => {
+            //change this to a filter instead of an alert but it is now recognising if the boxes are checked
+            alert(getSelectedCheckboxValues('beds'));
+        });
     });
 
 });
