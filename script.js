8f43d7e31ed4001e7a0ee84776e8519697cba3b5
Samantha Scibetta <53345649+scibettas1@users.noreply.github.com>
2021-02-19 16:56:39 -0500

    getting closer with the check box functionality but not there yet

--- script.js
+++ script.js
@@ -11,8 +11,6 @@ $(document).ready(() => {
         var result = JSON.parse(res);
         console.log(result)
 
-        // var beds= aptArray.beds
-
         var aptArray = result.map((apt) => {
 
             if (apt.Beds == 0) {
@@ -26,20 +24,39 @@ $(document).ready(() => {
             const sqft = apt.MinimumSQFT + "-" + apt.MaximumSQFT;
             const rent = "$" + apt.MinimumRent + "-$" + apt.MaximumRent;
             const plan = "<img src=https://cdn.rentcafe.com/dmslivecafe/2/70799/" + apt.FloorplanImageName + " class='img-fluid'>";
+            const availability = JSON.parse(apt.AvailableUnitsCount);
+            const url = apt.AvailabilityURL;
 
             const aptLi = "<div class='card shadow p-3 mb-5 bg-white rounded'><div class='row border-bottom'><h1 class='text-center'>" +
                 name + "</h1></div><div class='row'><div class='col-md-8'>" + plan + "</div><div class='col-md-4 details'><div class='row'>Beds: " +
                 beds + "</div><div class='row'>Baths: " + baths + "</div><div class='row'>SqFt: " +
-                sqft + "</div><div class='row'>Rent: " + rent + "</div></div></div></div>";
+                sqft + "</div><div class='row'>Rent: " + rent + "</div><div class='row'><a href=" + 
+                url + " target='_blank'><button class='float-left btn btn-light'>APPLY NOW</button></a></div></div></div></div>";
 
 
             //This only displays units if they are available
-            const availability = JSON.parse(apt.AvailableUnitsCount)
-            //console.log(availability)
-
             if (availability > 0) {
                 aptEl.append(aptLi);
             }
+
+            //This only displays units that are checked in the boxes. Work in progress.
+            const btn = document.querySelector('#btn');
+            btn.addEventListener('click', (event) => {
+                console.log("This is running.")
+                const bedrooms = getSelectedCheckboxValues('bedroom');
+                console.log(bedrooms);
+    
+                if (bedrooms !== "studio") {
+                    aptLi.classList.add("hide");
+                }
+                if (bedrooms !== "1") {
+                    aptLi.classList.add("hide");
+                }
+                if (bedrooms !== "2") {
+                    aptLi.classList.add("hide");
+                }
+    
+            });
 
             return {
                 name: name,
@@ -48,13 +65,11 @@ $(document).ready(() => {
                 sqft: sqft,
                 rent: rent,
                 plan: plan,
-                availability: availability
+                availability: availability,
+                url: url
             }
         })
         console.log(aptArray)
-
-        const bedrooms = getSelectedCheckboxValues('bedroom');
-        console.log(bedrooms);
 
         function getSelectedCheckboxValues(name) {
             const checkboxes = document.querySelectorAll(`input[name="${name}"]:checked`);
@@ -65,17 +80,23 @@ $(document).ready(() => {
             return values;
         }
 
-        const btn = document.querySelector('#btn');
-        btn.addEventListener('click', (event) => {
-            console.log("This is running.")
-            
+        // const btn = document.querySelector('#btn');
+        // btn.addEventListener('click', (event) => {
+        //     console.log("This is running.")
+        //     const bedrooms = getSelectedCheckboxValues('bedroom');
+        //     console.log(bedrooms);
 
-            // if (bedrooms === aptArray.beds) {
-            //     aptEl.append(aptLi);
-            // }
+        //     if (bedrooms !== "studio") {
+        //         aptLi.classList.add("hide")
+        //     }
+        //     if (bedrooms !== "1") {
+        //         aptLi.classList.add("hide")
+        //     }
+        //     if (bedrooms !== "2") {
+        //         aptLi.classList.add("hide")
+        //     }
 
-
-        });
+        // });
     });
 
 });
