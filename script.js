906c819a049902d3955418e3e8ceeba26905879e
Samantha Scibetta <53345649+scibettas1@users.noreply.github.com>
2021-02-23 13:22:36 -0500

    the search is now 100% functional.

--- script.js
+++ script.js
@@ -2,11 +2,15 @@
 $(document).ready(() => {
 
     $.ajax({
+        //100 House API that would be considered proprietary
         url: "https://api.rentcafe.com/rentcafeapi.aspx?requestType=floorplan&apiToken=a1599208-f362-437e-a0a5-71d260bcd755&VoyagerPropertyCode=house",
+        //Sample API provided in RentCafe's Documentaion
+        //url: "https://api.rentcafe.com/rentcafeapi.aspx?requestType=floorplan&apiToken=ODAxNw%3d%3d-0DHk4ex%2bpOU%3d&VoyagerPropertyCode=resca02",
         method: "GET",
 
     }).then((res) => {
-        const aptEl = $("#content")
+        const aptEl = $("#content");
+        const btn = $('#btn');
 
         const result = JSON.parse(res);
         //console.log(result)
@@ -28,7 +32,7 @@ $(document).ready(() => {
                 baths: apt.Baths,
                 sqft: apt.MinimumSQFT + "-" + apt.MaximumSQFT,
                 rent: "$" + apt.MinimumRent + "-$" + apt.MaximumRent,
-                plan: "<img src=https://cdn.rentcafe.com/dmslivecafe/2/70799/" + apt.FloorplanImageName + " class='img-fluid'>",
+                plan: "<img src=" + apt.FloorplanImageURL + " class='img-fluid'>",
                 availability: availability,
                 url: apt.AvailabilityURL
             }
@@ -47,7 +51,7 @@ $(document).ready(() => {
             const availability = apt.availability;
             const url = apt.url;
 
-            const aptLi = "<div class='card shadow p-3 mb-5 bg-white rounded'><div class='row border-bottom'><h1 class='text-center'>" +
+            const aptLi = "<div class='card shadow p-3 mb-5 bg-white rounded' id='aptLi'><div class='row border-bottom'><h1 class='text-center'>" +
                 name + "</h1></div><div class='row'><div class='col-md-8'>" + plan + "</div><div class='col-md-4 details'><div class='row'>Beds: " +
                 beds + "</div><div class='row'>Baths: " + baths + "</div><div class='row'>SqFt: " +
                 sqft + "</div><div class='row'>Rent: " + rent + "</div><div class='row'><a href=" +
@@ -57,30 +61,6 @@ $(document).ready(() => {
             if (availability > 0) {
                 aptEl.append(aptLi);
             }
-
-            // const btn = document.querySelector('#btn');
-            // btn.addEventListener('click', (event) => {
-            //     console.log("This is running.")
-            //     const bedrooms = getSelectedCheckboxValues('bedroom');
-                //console.log(bedrooms);
-
-                //This filters and displays units that are checked in the boxes. Work in progress.
-                // var aptFilter0 = aptArray.filter(apt => apt.beds == bedrooms[0]);
-                // var aptFilter1 = aptArray.filter(apt => apt.beds == bedrooms[1]);
-                // var aptFilter2 = aptArray.filter(apt => apt.beds == bedrooms[2]);
-
-                // var filterArray = aptFilter0.concat(aptFilter1, aptFilter2);
-
-                // var renderArray = filterArray.map((apt) => {
-                    // aptEl.remove(aptLi);
-
-                //     if (availability > 0) {
-                //         aptEl.append(aptLi);
-                //     }
-                // });
-                //console.log(filterArray)
-
-            // });
         });
 
         console.log(aptArray)
@@ -95,8 +75,8 @@ $(document).ready(() => {
             return values;
         }
 
-        const btn = document.querySelector('#btn');
-        btn.addEventListener('click', (event) => {
+
+        btn.on('click', (event) => {
             console.log("This is running.")
             const bedrooms = getSelectedCheckboxValues('bedroom');
             //console.log(bedrooms);
@@ -108,7 +88,12 @@ $(document).ready(() => {
 
             var filterArray = aptFilter0.concat(aptFilter1, aptFilter2);
 
+            for (var i = "0" ; i < aptArray.length; i++) {
+                $("#aptLi").remove();
+            }
+
             var renderArray = filterArray.map((apt) => {
+
                 const name = apt.name;
                 const beds = apt.beds;
                 const baths = apt.baths;
@@ -118,7 +103,7 @@ $(document).ready(() => {
                 const availability = apt.availability;
                 const url = apt.url;
 
-                const aptLi = "<div class='card shadow p-3 mb-5 bg-white rounded'><div class='row border-bottom'><h1 class='text-center'>" +
+                const aptLi = "<div class='card shadow p-3 mb-5 bg-white rounded' id='aptLi'><div class='row border-bottom'><h1 class='text-center'>" +
                     name + "</h1></div><div class='row'><div class='col-md-8'>" + plan + "</div><div class='col-md-4 details'><div class='row'>Beds: " +
                     beds + "</div><div class='row'>Baths: " + baths + "</div><div class='row'>SqFt: " +
                     sqft + "</div><div class='row'>Rent: " + rent + "</div><div class='row'><a href=" +
