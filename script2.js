61e971c99ea6fbc98d907ea3d52327ac4d212b9d
Samantha Scibetta <53345649+scibettas1@users.noreply.github.com>
2021-03-15 11:48:11 -0400

    updated api for a different property

--- script2.js
+++ script2.js
@@ -2,8 +2,8 @@
 $(document).ready(() => {
 
     $.ajax({
-        //100 House API that would be considered proprietary
-        url: "https://api.rentcafe.com/rentcafeapi.aspx?requestType=floorplan&apiToken=a1599208-f362-437e-a0a5-71d260bcd755&VoyagerPropertyCode=house",
+        //One Ten API that would be considered proprietary
+        url: "https://api.rentcafe.com/rentcafeapi.aspx?requestType=floorplan&apiToken=a1599208-f362-437e-a0a5-71d260bcd755&VoyagerPropertyCode=oneten",
         //Sample API provided in RentCafe's Documentaion
         //url: "https://api.rentcafe.com/rentcafeapi.aspx?requestType=floorplan&apiToken=ODAxNw%3d%3d-0DHk4ex%2bpOU%3d&VoyagerPropertyCode=resca02",
         method: "GET",
